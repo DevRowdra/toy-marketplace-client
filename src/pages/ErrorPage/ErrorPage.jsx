297e249346444a9018127428f2a42f59b63ee280
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-18 10:39:43 +0600

    errorpage work done

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import { Link, useRouteError } from 'react-router-dom';
+import errorpicture from '../../assets/3828541.jpg'
 const ErrorPage = () => {
+    const {status,data} = useRouteError();
+   
+    console.log(status,data)
     return (
         <div>
-            <h1>error</h1>
+        <div  >
+       
+        <img className='h-64  mx-auto'  src={errorpicture} alt="" />
         </div>
+        <div className='text-center'>
+        <h1 className='text-8xl text-emerald-300'>{status}</h1>
+        <p className='text-4xl'> {data}</p>
+       <Link to={'/'}> <button className="btn btn-success mt-5"> Home</button></Link>
+        </div>
+    </div>
     );
 };
 
