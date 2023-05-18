812956687e2d9963472120005298dac9955a6132
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-18 10:04:23 +0600

    user profile picture show on nevber

--- src/component/UserProfile/UserProfile.jsx
+++ src/component/UserProfile/UserProfile.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const UserProfile = () => {
+    const{user,photo,userName}=useContext(AuthContext)
+    console.log(user)
     return (
         <div>
-            <h1>user</h1>
+           {user && (
+              <div className="avatar relative">
+                <div className="w-12 rounded-full">
+                  <img src={user?.photoURL || photo} />
+                </div>
+
+                <div className="bg-black text-center bg-opacity-50 text-white rounded-full absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-200 ">
+                  <p
+                    className="relative top-3"
+                    title={user.displayName || userName}
+                  >
+                    {user?.displayName?.substring(0, 5) || userName.substring(0, 5)}
+                  </p>
+                </div>
+              </div>
+            )}
         </div>
     );
 };
