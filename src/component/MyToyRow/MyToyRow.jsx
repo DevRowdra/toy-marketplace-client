ad988d6da7afb52c015d6aa6e42b578e6b061fc5
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-19 09:20:03 +0600

    make dynamic my toy all item row

--- src/component/MyToyRow/MyToyRow.jsx
+++ src/component/MyToyRow/MyToyRow.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
-const MyToyRow = () => {
+import { FaBeer } from 'react-icons/fa';
+import { AiFillDelete, AiTwotoneEdit,  } from "react-icons/ai";
+const MyToyRow = ({toy}) => {
+    console.log(toy)
+    const{subCategory,sellerName,sellerEmail,rating
+,quantity,price
+,pictureUrl,name
+}=toy
     return (
         <tr>
         <th>
           <button className="btn btn-circle btn-outline" title='Edit'>
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
               fill="none"
@@ -18,47 +24,36 @@ const MyToyRow = () => {
                 strokeWidth="2"
                 d="M6 18L18 6M6 6l12 12"
               />
-            </svg>
+            </svg> */}
+            <AiTwotoneEdit className='text-2xl'></AiTwotoneEdit>
           </button>
         </th>
         <th>
           <button className="btn btn-circle btn-outline" title='Delete'>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
+          <AiFillDelete className='text-2xl'></AiFillDelete>
           </button>
         </th>
         <td>
     <div className="flex items-center space-x-3">
       <div className="avatar">
         <div className="mask mask-squircle w-12 h-12">
-          <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+          <img src={pictureUrl} alt="toy img" />
         </div>
       </div>
       <div>
-        <div className="font-bold">Hart Hagerty</div>
-        <div className="text-sm opacity-50">United States</div>
+        <div className="font-bold">{name}</div>
+      
       </div>
     </div>
   </td>
         
-        <td>Quality Control Specialist</td>
-        <td>Littel, Schaden and Vandervort</td>
-        <td>Canada</td>
-        <td>12/16/2020</td>
-        <td>Blue</td>
-        <td>Blue</td>
+        <td>{sellerName}</td>
+        <td>{sellerEmail}</td>
+        <td>{subCategory}</td>
+        <td>{price} $</td>
+        <td>{quantity}</td>
+        <td>{rating
+}</td>
       </tr>
     );
 };
